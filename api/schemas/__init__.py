4b316c7a4a339747d9e6ba381eeeec7fc5e31a3c
cphenrique <costa.phenrique@gmail.com>
2023-10-09 15:35:40 -0300

    Correção do __init__ dos schemas do Local

--- api/schemas/__init__.py
+++ api/schemas/__init__.py
@@ -1,4 +1,9 @@
+from schemas.error import ErrorSchema
+
 from schemas.atendimento.analista import AnalistaSchema, AnalistaViewSchema, ListagemAnalistasSchema, AnalistaBuscaPorNomeSchema, AnalistaBuscaPorIDSchema, AnalistaDelSchema, AnalistaBuscaSchema, apresenta_analistas, apresenta_analista
 from schemas.atendimento.processo import ProcessoSchema, ProcessoViewSchema, ListagemProcessosSchema, ProcessoBuscaPorNomeSchema, ProcessoBuscaPorIDSchema, ProcessoDelSchema, ProcessoBuscaSchema, apresenta_processos, apresenta_processo
 from schemas.atendimento.atividade import AtividadeSchema, AtividadeViewSchema, ListagemAtividadesSchema, AtividadeBuscaPorNomeSchema, AtividadeBuscaPorIDSchema, AtividadeDelSchema, AtividadeBuscaSchema, apresenta_atividades, apresenta_atividade
-from schemas.error import ErrorSchema
+
+from schemas.local.empresa import *
+from schemas.local.unidade import *
+from schemas.local.area import *
