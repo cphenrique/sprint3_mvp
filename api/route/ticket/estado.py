58da1e5893eb39c2c6d29b5bede454c0af2e5def
cphenrique <costa.phenrique@gmail.com>
2023-10-12 10:44:08 -0300

    Atualização das rotas de Estado

--- api/route/ticket/estado.py
+++ api/route/ticket/estado.py
@@ -33,111 +33,107 @@ def configure_estado_routes(app: Flask):
             return apresenta_estados(estados), 200
         
 
-    # @app.get('/analista', tags=[analista_tag],
-    #      responses={"200": AnalistaViewSchema, "404": ErrorSchema})
-    # def get_analista(query: AnalistaBuscaPorIDSchema):
-    #     """Faz a busca por um Produto a partir do id do produto
+    @app.get('/estado', tags=[estado_tag],
+         responses={"200": EstadoViewSchema, "404": ErrorSchema})
+    def get_estado(query: EstadoBuscaSchema):
+        """Faz a busca por um Produto a partir do id do produto
 
-    #     Retorna uma representação dos produtos e comentários associados.
-    #     """
-    #     analista_id = query.id
-    #     logger.info(f"Coletando dados sobre o Analista #{analista_id}")
-    #     # criando conexão com a base
-    #     session = Session()
-    #     # fazendo a busca
-    #     analista = session.query(Analista).filter(Analista.id == analista_id).first()
+        Retorna uma representação dos produtos e comentários associados.
+        """
+        estado_id = query.id
+        logger.info(f"Coletando dados sobre o Estado #{estado_id}")
+        # criando conexão com a base
+        session = Session()
+        # fazendo a busca
+        estado = session.query(Estado).filter(Estado.id == estado_id).first()
 
-    #     if not analista:
-    #         # se o produto não foi encontrado
-    #         error_msg = "Analista não encontrado na base"
-    #         logger.warning(f"Erro ao buscar o Analista '{analista_id}', {error_msg}")
-    #         return {"mesage": error_msg}, 404
-    #     else:
-    #         logger.info("Analista econtrado: %s" % analista)
-    #         # retorna a representação de produto
-    #         return apresenta_analista(analista), 200
+        if not estado:
+            # se o produto não foi encontrado
+            error_msg = "Estado não encontrado na base"
+            logger.warning(f"Erro ao buscar o Estado '{estado_id}', {error_msg}")
+            return {"mesage": error_msg}, 404
+        else:
+            logger.info("Estado econtrado: %s" % estado)
+            # retorna a representação de produto
+            return apresenta_estado(estado), 200
 
 
-    # @app.post('/analista', tags=[analista_tag],
-    #         responses={"200": AnalistaViewSchema, "409": ErrorSchema, "400": ErrorSchema})
-    # def add_analista(form: AnalistaSchema):
-    #     """ Adiciona um novo projeto a base de dados.
+    @app.post('/estado', tags=[estado_tag],
+            responses={"200": EstadoViewSchema, "409": ErrorSchema, "400": ErrorSchema})
+    def add_estado(form: EstadoSchema):
+        """ Adiciona um novo projeto a base de dados.
 
-    #     Retorna para uma representação dos projetos e atividades relacionadas.
-    #     """
-    #     analista = Analista(
-    #         nome=form.nome,
-    #         usuario=form.usuario,
-    #         email=form.email
-    #         )
-    #     logger.debug(f"Adicionando o analista '{analista.nome} na base de dados")
-    #     try:
-    #         # criando conexão com a base de dados
-    #         session = Session()
-    #         # adicionando projeto
-    #         session.add(analista)
-    #         # efetivando o comando de add novo item na tabela
-    #         session.commit()
-    #         logger.debug(f"{analista.nome} adicionado com sucesso.")
-    #         return apresenta_analista(analista), 200
+        Retorna para uma representação dos projetos e atividades relacionadas.
+        """
+        estado = Estado(
+            estado=form.estado
+            )
+        logger.debug(f"Adicionando o Estado '{estado.estado} na base de dados")
+        try:
+            # criando conexão com a base de dados
+            session = Session()
+            # adicionando projeto
+            session.add(estado)
+            # efetivando o comando de add novo item na tabela
+            session.commit()
+            logger.debug(f"{estado.estado} adicionado com sucesso.")
+            return apresenta_estado(estado), 200
         
-    #     except Exception as e:
-    #         # tratamento de erros
-    #         error_msg = "Não foi possível adicionar o analista"
-    #         logger.warning(f"Erro ao adicionar o analista '{analista.nome}', {error_msg}")
-    #         return {"message": error_msg}, 400
+        except Exception as e:
+            # tratamento de erros
+            error_msg = "Não foi possível adicionar o Estado"
+            logger.warning(f"Erro ao adicionar o Estado '{estado.estado}', {error_msg}")
+            return {"message": error_msg}, 400
 
 
-    # @app.put('/analista', tags=[analista_tag],
-    #             responses={"200": AnalistaViewSchema, "404": ErrorSchema})
-    # def put_analista(query: AnalistaBuscaSchema, form: AnalistaSchema):
-    #     """Edita um Carro a partir do id do carro informado
+    @app.put('/estado', tags=[estado_tag],
+                responses={"200": EstadoViewSchema, "404": ErrorSchema})
+    def put_estado(query: EstadoBuscaSchema, form: EstadoSchema):
+        """Edita um Carro a partir do id do carro informado
 
-    #     Retorna uma mensagem de confirmação da remoção.
-    #     """
-    #     analista_id = query.id
-    #     logger.debug(f"Coletando dados sobre o Analista #{analista_id}")
-    #     # criando conexão com a base
-    #     session = Session()
-    #     # fazendo a busca
-    #     analista = session.query(Analista).filter(Analista.id == analista_id).first()
+        Retorna uma mensagem de confirmação da remoção.
+        """
+        estado_id = query.id
+        logger.debug(f"Coletando dados sobre o Estado #{estado_id}")
+        # criando conexão com a base
+        session = Session()
+        # fazendo a busca
+        estado = session.query(Estado).filter(Estado.id == estado_id).first()
 
-    #     if not analista:
-    #         # se o carro não foi encontrado
-    #         error_msg = "Analista não encontrado na base"
-    #         logger.warning(f"Erro ao editar o Analista #'{analista_id}', {error_msg}")
-    #         return {"mesage": error_msg}, 404
-    #     else:
-    #         # edita o carro e retorna a representação
-    #         logger.info("Alterando informações do Analista: %s" % analista)
-    #         analista.nome=form.nome
-    #         analista.usuario=form.usuario
-    #         analista.email=form.email
-    #         session.commit()
-    #         return apresenta_analista(analista), 200
+        if not estado:
+            # se o carro não foi encontrado
+            error_msg = "Estado não encontrado na base"
+            logger.warning(f"Erro ao editar o Estado #'{estado_id}', {error_msg}")
+            return {"mesage": error_msg}, 404
+        else:
+            # edita o carro e retorna a representação
+            logger.info("Alterando informações do Estado: %s" % estado)
+            estado.estado=form.estado
+            session.commit()
+            return apresenta_estado(estado), 200
 
 
-    # @app.delete('/analista', tags=[analista_tag],
-    #             responses={"200": AnalistaDelSchema, "404": ErrorSchema})
-    # def del_analista(query: AnalistaBuscaSchema):
-    #     """Deleta um Projeto a partir do nome do projeto informado
+    @app.delete('/estado', tags=[estado_id],
+                responses={"200": EstadoDelSchema, "404": ErrorSchema})
+    def del_estado(query: EstadoBuscaSchema):
+        """Deleta um Projeto a partir do nome do projeto informado
 
-    #     Retorna uma mensagem de confirmação da remoção.
-    #     """
-    #     analista_id = query.id
-    #     logger.debug(f"Deletando dados sobre analista #{analista_id}")
-    #     # criando conexão com a base
-    #     session = Session()
-    #     # fazendo a remoção
-    #     count = session.query(Analista).filter(Analista.id == analista_id).delete()
-    #     session.commit()
+        Retorna uma mensagem de confirmação da remoção.
+        """
+        estado_id = query.id
+        logger.debug(f"Deletando dados sobre Estado #{estado_id}")
+        # criando conexão com a base
+        session = Session()
+        # fazendo a remoção
+        count = session.query(Estado).filter(Estado.id == estado_id).delete()
+        session.commit()
 
-    #     if count:
-    #         # retorna a representação da mensagem de confirmação
-    #         logger.debug(f"Deletado o Analista #{analista_id}")
-    #         return {"mesage": "Analista removido", "id": analista_id}
-    #     else:
-    #         # se o projeto não foi encontrado
-    #         error_msg = "Analista não encontrado na base"
-    #         logger.warning(f"Erro ao deletar o Analista #'{analista_id}', {error_msg}")
-    #         return {"mesage": error_msg}, 404
+        if count:
+            # retorna a representação da mensagem de confirmação
+            logger.debug(f"Deletado o Estado #{estado_id}")
+            return {"mesage": "Estado removido", "id": estado_id}
+        else:
+            # se o projeto não foi encontrado
+            error_msg = "Estado não encontrado na base"
+            logger.warning(f"Erro ao deletar o Estado #'{estado_id}', {error_msg}")
+            return {"mesage": error_msg}, 404
